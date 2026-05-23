5b0a190a21dc99781b752acc86976da368c55cac
David Chin <dlcmhd@me.com>
2026-05-23 16:19:46 +0800

    add chapter 1 code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("Asteroids")]
+async fn main() {
+    loop {
+        // 1. Process input
+        // 2. Update state
+        // 3. Draw everything
+
+        clear_background(BLACK);
+        draw_text("Hello, Asteroids!", 20.0, 20.0, 30.0, WHITE);
+
+        next_frame().await;
+    }
 }
